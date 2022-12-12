383f66425f2c41ddf55b14579103e41ff314021c
Benjamin Polin <57519356+Benhatsdrauf@users.noreply.github.com>
2022-12-12 11:18:37 +0100

    add logout functionality

--- MuehleOnline/src/pages/HomePage.svelte
+++ MuehleOnline/src/pages/HomePage.svelte
@@ -1,2 +1,32 @@
+<script>
+    import { useNavigate } from "svelte-navigator";
+
+    const navigate = useNavigate();
+
+    async function Logout() {
+        await fetch("http://localhost:420/logout", {
+            method: "Post",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        })
+            .then(() => {
+                localStorage.removeItem("token");
+                navigate("/");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+</script>
+
 <h3>Home</h3>
 <p>This is gonna be the home/statistics page after the user is logged in</p>
+
+<button on:click={Logout}>Logout</button>
+
+<style>
+</style>

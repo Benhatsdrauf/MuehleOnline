47561a81073b43f9ad98e95f7518e85267cdd2f1
Benjamin Polin <57519356+Benhatsdrauf@users.noreply.github.com>
2022-12-12 14:32:12 +0100

    Update Modal.svelte

--- MuehleOnline/src/lib/Modal.svelte
+++ MuehleOnline/src/lib/Modal.svelte
@@ -39,7 +39,7 @@
     <hr />
 
     <!-- svelte-ignore a11y-autofocus -->
-    <button autofocus on:click={close}>close modal</button>
+    <button autofocus on:click={close}>X</button>
 </div>
 
 <style>

a537429bf1f558fbc2726abba77aa4c6cc5c1491
Benjamin Polin <57519356+Benhatsdrauf@users.noreply.github.com>
2022-12-12 11:18:16 +0100

    add register functionality

--- MuehleOnline/src/lib/Panels/RegisterPanel.svelte
+++ MuehleOnline/src/lib/Panels/RegisterPanel.svelte
@@ -1,19 +1,45 @@
 <script>
+    import { useNavigate } from "svelte-navigator";
+
+    const navigate = useNavigate();
+
     let userName = "";
     let password = "";
-    let confirmPassword = "";
+
+    async function Register() {
+        const data = {
+            name: userName,
+            pw: password,
+        };
+        await fetch("http://localhost:420/register", {
+            method: "Post",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                const token = data.token;
+                localStorage.setItem("token", token);
+                navigate("home");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+
+        console.log(localStorage.getItem("token"));
+    }
 </script>
 
 <h1>Register Panel</h1>
 
 <input type="text" placeholder="Username" bind:value={userName} />
 <input type="password" placeholder="Password" bind:value={password} />
-<input
-    type="password"
-    placeholder="confirm Password"
-    bind:value={confirmPassword}
-/>
-<button type="button">Register</button>
+
+<button type="button" on:click={Register}>Register</button>
 
 <style>
 </style>

9dc627701ddb61ce3b88999d4538f3ee1e20ccad
Felix <rhomberg.felix03@gmail.com>
2022-12-12 16:20:03 +0100

    added move event

--- Backend/config/broadcasting.php
+++ Backend/config/broadcasting.php
@@ -36,6 +36,7 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => env('PUSHER_HOST', '127.0.0.1'),
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),

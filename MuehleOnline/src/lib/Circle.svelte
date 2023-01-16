3546ecd6074d36cefbcb6afb65cf84e892bf4e71
Benjamin Polin <57519356+Benhatsdrauf@users.noreply.github.com>
2023-01-16 15:27:25 +0100

    Create Circle.svelte

---
+++ MuehleOnline/src/lib/Circle.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let x = 0;
+    export let y = 0;
+    export let index = 0;
+    export let status = "initial";
+</script>
+
+<circle
+    on:click={() => {
+        console.log(index);
+    }}
+    cx="{x}%"
+    cy="{y}%"
+    r="15"
+/>
+
+<style>
+    circle {
+        cursor: pointer;
+    }
+</style>

fc70e60cf4213c1d9a962254700659fcc8a08ee9
Benjamin Polin <57519356+Benhatsdrauf@users.noreply.github.com>
2023-01-23 10:29:17 +0100

    new component

---
+++ MuehleOnline/src/lib/GameField.svelte
@@ -0,0 +1,31 @@
+<rect
+    x="5%"
+    y="5%"
+    width="90%"
+    height="90%"
+    fill="none"
+    stroke="black"
+    stroke-width="5"
+/>
+<rect
+    x="20%"
+    y="20%"
+    width="60%"
+    height="60%"
+    fill="none"
+    stroke="black"
+    stroke-width="5"
+/>
+<rect
+    x="35%"
+    y="35%"
+    width="30%"
+    height="30%"
+    fill="none"
+    stroke="black"
+    stroke-width="5"
+/>
+<line x1="5%" y1="50%" x2="35%" y2="50%" stroke="black" stroke-width="5" />
+<line x1="65%" y1="50%" x2="95%" y2="50%" stroke="black" stroke-width="5" />
+<line x1="50%" y1="5%" x2="50%" y2="35%" stroke="black" stroke-width="5" />
+<line x1="50%" y1="65%" x2="50%" y2="95%" stroke="black" stroke-width="5" />

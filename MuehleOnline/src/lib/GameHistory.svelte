bacbf61fb28aa39a513f4742cd247237b20ad7e0
Felix <rhomberg.felix03@gmail.com>
2023-02-06 11:18:05 +0100

    reworked game history design

--- MuehleOnline/src/lib/GameHistory.svelte
+++ MuehleOnline/src/lib/GameHistory.svelte
@@ -1,32 +1,37 @@
 <script>
+  import { faBolt, faBurst, faCrown, faExplosion, faL } from "@fortawesome/free-solid-svg-icons";
+  import Fa from "svelte-fa";
+
     export let won = false;
     export let opponent = "";
     export let playtime = 0;
-    export let count = 0;
     export let username = "";
     export let start_date = "";
     export let elo = 0;
 
     let date = new Date(start_date);
 </script>
-
-<div class="card" class:bg-success="{won}" class:bg-danger="{!won}">
+<div class="card">
     <div class="card-body">
         <div class="row">
-            <div class="col-auto right-border c-text">
-                {count}
+            <div class="col-auto right-border">
+                {#if won}
+                <Fa icon={faCrown} color="var(--color-win)" size="1.6x" />
+                {:else}
+                <Fa icon={faBolt} color="var(--color-los)" size="2x" />
+                {/if}
             </div>
-            <div class="col c-text">
-                {username} VS {opponent}
+            <div class="col-auto">
+                <b>{opponent}</b>
             </div>
-            <div class="col c-text text-center">
-                {(won) ? "+" : ""}{elo}
+            <div class="col text-center" class:text-win="{won}" class:text-los="{!won}">
+                <b>{(won) ? "+" : ""}{elo}</b>
             </div>
-            <div class="col c-text text-center">
-                Play time: {new Date(playtime * 1000).toISOString().slice(11, 19)}
+            <div class="col text-center">
+                Play time: <b>{new Date(playtime * 1000).toISOString().slice(11, 19)}</b>
             </div>
-            <div class="col c-text text-end">
-                {`${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`}
+            <div class="col text-end">
+                <b>{`${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`}</b>
             </div>
         </div>
     </div>
@@ -36,11 +41,19 @@
 
     .right-border
     {
-        border-right: solid var(--color-white) 2px;
+        border-right: solid var(--color-black) 2px;
     }
 
     .card{
         border: solid var(--color-black) 2px;
+    }
+    
+    .text-los{
+        color: var(--color-los)
+    }
+
+    .text-win {
+        color: var(--color-win)
     }
 
 </style>

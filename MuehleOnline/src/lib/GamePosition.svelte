c7b44fac82021a32da5d9dd05e64b74c9c440c7b
Benjamin Polin <57519356+Benhatsdrauf@users.noreply.github.com>
2023-02-06 11:09:56 +0100

    Create GamePosition.svelte

---
+++ MuehleOnline/src/lib/GamePosition.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Circle from "./Circle.svelte";
+    export let isPossible = false;
+    export let isDisabled = false;
+    export let position = [];
+</script>
+
+<Circle x={position[0]} y={position[1]} {isPossible} {isDisabled} on:click />
